2ce824833ea9dcab6b40326781300e6fde6684aa
gennabarge <gennabarge@10.0.0.12>
2019-03-01 11:53:02 -0500

    Finished board for player one

--- src/Battleship.java
+++ src/Battleship.java
@@ -3,16 +3,20 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 public class Battleship extends JFrame {
 		private static final int GRIDSIZE = 10; 
 		private static final int NUMBEROFSHIPSPACES = 12;
-		private Battleship[] [] terrain = new Battleship [GRIDSIZE] [GRIDSIZE]; 
+		private Ships[] [] water = new Ships [GRIDSIZE] [GRIDSIZE]; 
 		private int totalHit = 0; 
 		
 		public Battleship() {
@@ -40,6 +44,75 @@ public class Battleship extends JFrame {
 			JPanel centerPanel = new JPanel();
 			centerPanel.setLayout(new GridLayout(GRIDSIZE, GRIDSIZE));
 			add(centerPanel, BorderLayout.CENTER);
+			for (int r = 0; r < GRIDSIZE; r++) {
+				for (int c = 0; c < GRIDSIZE; c++) {
+					water[r][c] = new Ships(r,c);
+					water[r][c].addActionListener(new ActionListener(){
+						public void actionPerformed (ActionEvent e) {
+							Ships button = (Ships) e.getSource();
+							int row = button.getRow();
+							int col = button.getCol();
+							spaceClicked(row,col);
+						}
+					});
+					centerPanel.add(water[r][c]);
+				} 
+			} 
+		}
+		private void spaceClicked (int row, int col) {
+			if (water[row][col].hasShip()) {
+				totalHit += 1;
+				water[row][col].reveal(true);
+				
+				if (totalHit == NUMBEROFSHIPSPACES) {
+				String message = "You win! Do you want to play again?";
+				promptForNewGame(message);
+			
+		} }
+		}
+		private void setShips() {
+			Random rand = new Random();
+			int pickRow;
+			int pickCol;
+			for (int i = 0; i < NUMBEROFSHIPSPACES; i++) {
+				do {
+					pickRow = rand.nextInt(GRIDSIZE);
+					pickCol = rand.nextInt(GRIDSIZE);
+					
+				}
+				while (water[pickRow][pickCol].hasShip());
+				water[pickRow][pickCol].setShip(true);
+			}
+				//addToNeighborsHoleCount(pickRow, pickCol);
+				//terrain[pickRow][pickCol].reveal(true);
+			}
+		private void showShips() {
+			for (int row = 0; row <GRIDSIZE; row++){
+				for ( int col = 0; col <GRIDSIZE; col++  ) {
+					if (water[row][col].hasShip()) {
+						water[row][col].reveal(true);
+					}
+				}
+			}
+		}
+		private void promptForNewGame(String mess) {
+			showShips();
+			int option = JOptionPane.showConfirmDialog(this, mess, "Play Again?", JOptionPane.YES_NO_OPTION);
+			if (option == JOptionPane.YES_OPTION) {
+				newGame();
+			}
+			else {
+				System.exit(0);
+			}
+			
+		}
+		private void newGame() {
+			for (int row = 0; row <GRIDSIZE; row++){
+				for ( int col = 0; col <GRIDSIZE; col++  ) {
+					water[row][col].reset(); 
+				} }
+			setShips(); 
+			totalHit = 0; 
 		}
 	
 	
--- src/Ships.java
+++ src/Ships.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JButton;
@@ -8,6 +9,8 @@ public class Ships extends JButton{
 		private int col = 0;
 		private boolean shipHit = false;
 		private boolean shipRevealed = false; 
+		private int totalHit = 0;
+		
 
 
 public Ships (int r, int c) {
@@ -18,5 +21,40 @@ public Ships (int r, int c) {
 }
 public void setShip(boolean hasShip) {
 	shipHit = hasShip; 
+}
+public void increaseTotalHit() {
+	totalHit += 1; 
+}
+
+public void reveal (boolean reveal) {
+	shipRevealed = reveal; 
+	if (shipRevealed == true) {
+		if (shipHit == true) {
+		setBackground(Color.BLACK); 	
+	}
+		else {
+			setBackground(Color.CYAN); 
+		}
+		
+		
+		
+		
+	}		
+		
+		
+}
+public void reset() {
+	shipHit = false;
+	shipRevealed = false;
+	setBackground(null); 
+}
+public boolean hasShip() {
+	return shipHit;
+}
+public int getRow() {
+	return row;
+		}
+public int getCol() {
+	return col;
 }
 }
